42fb72f3262039464b1854d69b33702e65e0408a
sudeep987 <msudeep.joel@gmail.com>
2021-02-02 23:56:41 +0530

    readme and minor changes

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ export const LOGIN='login'
 export const LOGOUT='logout'
 export const SET_USERS='setusers'
 export const DEVSERVER='https://uviz4022j1.execute-api.us-east-1.amazonaws.com';
-export const PRODSERVER='https://uviz4022j1.execute-api.us-east-1.amazonaws.com';
+export const PRODSERVER='https://npoeootl24.execute-api.us-east-1.amazonaws.com';
 export const LOCALSERVER='http://localhost:5000';
 
-export const SERVER=DEVSERVER;
+export const SERVER=PRODSERVER;
